1006e64f19a9ef2a3903553efbcfaf883134f155
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-08-01 00:15:01 +0300

    mock topic group with its threads

---
+++ app/controllers/topic_group_controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class TopicGroupController extends BaseController{
+
+    public static function topicGroup1() {
+        View::make('topic-groups/topic_group.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -3,5 +3,7 @@
   $routes -> get('/', function() {
       MainController::index();
   });
-
+  $routes-> get('/topic-groups/1', function() {
+      TopicGroupController::topicGroup1();
+  });
 
