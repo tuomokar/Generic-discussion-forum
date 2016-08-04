0dbdbdd4c1c5806d754d3b6e547cb09ff92c0c43
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-08-04 17:37:03 +0300

    mockup views for thread editing and creation

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -6,4 +6,12 @@ class ThreadController extends BaseController{
         View::make('threads/thread_show.html');
     }
 
+    public static function threadNew() {
+        View::make('threads/thread_new.html');
+    }
+
+    public static function threadEdit() {
+        View::make('threads/thread_edit.html');
+    }
+
 }
--- config/routes.php
+++ config/routes.php
@@ -4,10 +4,21 @@ $routes -> get('/', function() {
     MainController::index();
 });
 
+// temp address
 $routes -> get('/topic-groups/1', function() {
     TopicGroupController::topicGroupShow();
 });
 
+// temp address
 $routes -> get('/threads/1', function() {
     ThreadController::threadShow();
+});
+
+// temp address
+$routes -> get('/threads/1/edit', function() {
+    ThreadController::threadEdit();
+});
+
+$routes -> get('/threads/new', function() {
+    ThreadController::threadNew();
 });
