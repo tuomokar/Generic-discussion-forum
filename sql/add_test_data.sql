3f1160e5f29753516ed23215650acc0f9f8e96d6
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-08-08 23:43:27 +0300

    create tables

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,3 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Forum_user (username, password, info, created, edited) VALUES ('user1', 'user1', 'Man, 40-year-old, accountant', 2016-07-31, 2016-07-31);
+INSERT INTO Forum_user (username, password, info, created, edited) VALUES ('user2', 'user2', 'Woman, 41 years old, IT worker', 2016-07-31, 2016-07-31);
+INSERT INTO Forum_user (username, password, info, created, edited) VALUES ('user3', 'user3', 'Man, 39-year-old, drunk', 2016-07-31, 2016-07-31);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,45 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Forum_user(
+  id SERIAL PRIMARY KEY,
+  username varchar(50) NOT NULL,
+  password varchar(100) NOT NULL,
+  info varchar(400),
+  created date,
+  edited date
+);
+
+CREATE TABLE User_group(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  info varchar(400) NOT NULL,
+  created date,
+  edited date
+);
+
+CREATE TABLE Membership(
+  forum_user_id INTEGER REFERENCES Forum_user(id),
+  user_group_id INTEGER REFERENCES User_group(id)
+);
+
+CREATE TABLE Topic_group(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  info varchar(400) NOT NULL,
+  created DATE,
+  edited DATE
+);
+
+CREATE TABLE Thread(
+  id SERIAL PRIMARY KEY,
+  topic_group_id INTEGER REFERENCES Topic_group(id),
+  title VARCHAR(50),
+  created DATE,
+  edited DATE
+);
+
+CREATE TABLE Post(
+  user_id INTEGER REFERENCES Forum_user(id),
+  thread_id INTEGER REFERENCES Thread(id),
+  message TEXT,
+  created DATE,
+  edited DATE
+);
