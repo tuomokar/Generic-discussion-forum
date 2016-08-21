7049b20021c8cc1cb0c301647ee6c1dd4c6eef82
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-08-21 12:42:17 +0300

    confirms deletion of anything

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  //alert('Hello World!');
+    $('form.destroy-form').on('submit', function(submit){
+        var confirm_message = "Are you sure? Object will be removed permanently.";
+        if(!confirm(confirm_message)){
+            submit.preventDefault();
+        }
+    });
 });
